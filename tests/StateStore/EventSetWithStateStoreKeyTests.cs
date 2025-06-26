0388f0c4002d0cafa69a769490271b347aa752bb
sage-mt07 <65053888+sage-mt07@users.noreply.github.com>
2025-06-27 07:28:12 +0900

    Fix missing CancellationToken imports

--- tests/StateStore/EventSetWithStateStoreKeyTests.cs
+++ tests/StateStore/EventSetWithStateStoreKeyTests.cs
@@ -3,6 +3,7 @@ using Kafka.Ksql.Linq.Core.Abstractions;
 using Kafka.Ksql.Linq.StateStore;
 using System;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
 
