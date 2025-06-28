de491a4cf47176bb3eeb8cddee446b4e298359ff
sage-mt07 <65053888+sage-mt07@users.noreply.github.com>
2025-06-28 23:18:46 +0900

    Fix final topic unit tests

--- tests/Window/Finalization/UseFinalizedDslTests.cs
+++ tests/Window/Finalization/UseFinalizedDslTests.cs
@@ -17,16 +17,23 @@ public class UseFinalizedDslTests
     {
         private class StubSet<T> : IEntitySet<T> where T : class
         {
+            private readonly IKsqlContext _context;
+
+            public StubSet(IKsqlContext context)
+            {
+                _context = context;
+            }
+
             public Task AddAsync(T entity, CancellationToken cancellationToken = default) => Task.CompletedTask;
             public Task<List<T>> ToListAsync(CancellationToken cancellationToken = default) => Task.FromResult(new List<T>());
             public Task ForEachAsync(Func<T, Task> action, TimeSpan timeout = default, CancellationToken cancellationToken = default) => Task.CompletedTask;
             public string GetTopicName() => typeof(T).Name;
             public EntityModel GetEntityModel() => new EntityModel { EntityType = typeof(T), TopicAttribute = new TopicAttribute("orders"), AllProperties = typeof(T).GetProperties(), KeyProperties = new[] { typeof(T).GetProperty("Id")! } };
-            public IKsqlContext GetContext() => null!;
+            public IKsqlContext GetContext() => _context;
             public async IAsyncEnumerator<T> GetAsyncEnumerator(CancellationToken cancellationToken = default) { await Task.CompletedTask; yield break; }
         }
 
-        public IEntitySet<T> Set<T>() where T : class => new StubSet<T>();
+        public IEntitySet<T> Set<T>() where T : class => new StubSet<T>(this);
         public object GetEventSet(Type entityType) => throw new NotImplementedException();
         public Dictionary<Type, EntityModel> GetEntityModels() => new();
         public void Dispose() { }
