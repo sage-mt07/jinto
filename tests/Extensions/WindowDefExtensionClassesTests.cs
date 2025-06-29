aa9a9772060d68816aeaebec6569c6eaf58095c0
sage-mt07 <65053888+sage-mt07@users.noreply.github.com>
2025-06-29 15:23:21 +0900

    Fix namespace conflicts in window extension tests

--- tests/Extensions/WindowDefExtensionClassesTests.cs
+++ tests/Extensions/WindowDefExtensionClassesTests.cs
@@ -2,16 +2,16 @@ using System;
 using System.Linq;
 using System.Reflection;
 using System.Collections.Generic;
-using Kafka.Ksql.Linq;
+using Ksql = Kafka.Ksql.Linq;
 using Xunit;
 
 namespace Kafka.Ksql.Linq.Tests.Extensions;
 
 public class WindowDefExtensionClassesTests
 {
-    private static List<(string Name, object? Value)> GetOperations(WindowDef def)
+    private static List<(string Name, object? Value)> GetOperations(Ksql.WindowDef def)
     {
-        var field = typeof(WindowDef).GetField("Operations", BindingFlags.Instance | BindingFlags.NonPublic)!;
+        var field = typeof(Ksql.WindowDef).GetField("Operations", BindingFlags.Instance | BindingFlags.NonPublic)!;
         return (List<(string Name, object? Value)>)field.GetValue(def)!;
     }
 
@@ -19,7 +19,7 @@ public class WindowDefExtensionClassesTests
     public void HoppingWindow_Of_BuildsOperations()
     {
         var ts = TimeSpan.FromMinutes(5);
-        var def = HoppingWindow.Of(ts);
+        var def = Ksql.HoppingWindow.Of(ts);
         var ops = GetOperations(def);
         Assert.Equal(new[] { ("HoppingWindow", (object?)null), ("Size", (object?)ts) }, ops);
     }
@@ -27,7 +27,7 @@ public class WindowDefExtensionClassesTests
     [Fact]
     public void HoppingWindow_OfMinutes_BuildsOperations()
     {
-        var def = HoppingWindow.OfMinutes(2);
+        var def = Ksql.HoppingWindow.OfMinutes(2);
         var ts = TimeSpan.FromMinutes(2);
         var ops = GetOperations(def);
         Assert.Equal(new[] { ("HoppingWindow", (object?)null), ("Size", (object?)ts) }, ops);
@@ -37,7 +37,7 @@ public class WindowDefExtensionClassesTests
     public void SessionWindow_Of_BuildsOperations()
     {
         var ts = TimeSpan.FromMinutes(7);
-        var def = SessionWindow.Of(ts);
+        var def = Ksql.SessionWindow.Of(ts);
         var ops = GetOperations(def);
         Assert.Equal(new[] { ("SessionWindow", (object?)null), ("Gap", (object?)ts) }, ops);
     }
@@ -45,7 +45,7 @@ public class WindowDefExtensionClassesTests
     [Fact]
     public void SessionWindow_OfMinutes_BuildsOperations()
     {
-        var def = SessionWindow.OfMinutes(3);
+        var def = Ksql.SessionWindow.OfMinutes(3);
         var ts = TimeSpan.FromMinutes(3);
         var ops = GetOperations(def);
         Assert.Equal(new[] { ("SessionWindow", (object?)null), ("Gap", (object?)ts) }, ops);
